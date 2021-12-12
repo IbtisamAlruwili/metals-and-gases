3b9e91e52e0767c058b4ea07b60c586ee23d9e1e
IbtisamAlruwili <Ibtisam.Ghazi.rae.js@tuwaiq.edu.sa>
2021-12-12 16:04:53 +0300

    backend

---
+++ backend/db/models/Admin.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const adminModel = new mongoose.Schema({
+  name: { type: String },
+  email: { type: String, unique: true },
+  password: { type: String },
+  OurWork: [{type: mongoose.Schema.Types.ObjectId, ref: "productModel"}] ,
+  Metal:   [{type: mongoose.Schema.Types.ObjectId, ref: "productModel"}] ,
+  Gases:   [{type: mongoose.Schema.Types.ObjectId, ref: "productModel"}] ,
+  Like:    [{type: mongoose.Schema.Types.ObjectId, ref: "productModel"}] ,
+});
+
+module.exports = mongoose.model("adminModel", adminModel);
---
+++ backend/db/models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const userModel = new mongoose.Schema({
+  name: { type: String },
+  email: { type: String, unique: true },
+  password: { type: String },
+  OurWork:[{type: mongoose.Schema.Types.ObjectId, ref: "productModel"}] ,
+  Like:  [{type: mongoose.Schema.Types.ObjectId, ref: "productModel"}] ,
+});
+
+module.exports = mongoose.model("userModel", userModel);
